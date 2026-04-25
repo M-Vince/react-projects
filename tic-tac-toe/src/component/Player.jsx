0267c486a03b3fa84d8440653e38ccaab377dde6
M-Vince <165883810+M-Vince@users.noreply.github.com>
2026-04-25 23:36:02 +0800

    feat: Add name editing functionality

--- tic-tac-toe/src/component/Player.jsx
+++ tic-tac-toe/src/component/Player.jsx
@@ -1,18 +1,47 @@
 import React from "react";
+import { useState } from "react";
 
-export default function Player({playerName, symbol}) {
+export default function Player({ playerName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [changeName, setChangeName] = useState(playerName);
+
+  const handleEditButton = () => {
+    setIsEditing(!isEditing);
+  }
+
   return (
     <li className="flex items-center w-[50%] border-2 border-solid border-transparent">
       <span className="border-2 border-solid border-transparent p-2 rounded-sm font-bold">
-        <span className="inline-block w-40 text-xl text-[#e1dec7] uppercase m-0 p-2 rounded-sm text-ellipsis text-center">
-          {playerName}
-        </span>
-        <span className="ml-4 text-xl text-[#e1dec7]">
-            {symbol}
-        </span>
+        {
+          // If true, display edit mode . If false, display player's name. 
+          isEditing ?
+          <>
+            <span className="w-40 p-2 bg-[#46432f] text-[#e1dec7] text-lg text-center uppercase border-none animate-pulse ">
+              <input
+                type="text"
+                onChange={(e) => {
+                  setChangeName(e.target.value);
+                }}
+              />
+            </span>
+          </>
+          :
+          <>
+            <span className="inline-block w-40 text-xl text-[#e1dec7] uppercase m-0 p-2 rounded-sm text-ellipsis text-center">
+              {changeName}
+            </span>
+            <span className="ml-4 text-xl text-[#e1dec7]">{symbol}</span>
+          </>
+        }
+        
       </span>
 
-      <button className="w-12 border-none bg-none text-[#c3ba78] text-sm cursor-pointer transition-colors duration-200 px-1 pt-2 pb-2 hover:text-[#f8ca31]"> Edit </button>
+      <button
+        className="w-12 border-none bg-none text-[#c3ba78] text-sm cursor-pointer transition-colors duration-200 px-1 pt-2 pb-2 hover:text-[#f8ca31]"
+        onClick={handleEditButton}
+      >
+        {isEditing ? "Save" : "Edit"}
+      </button>
     </li>
   );
 }
